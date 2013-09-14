d193ce369e23e6eda33a2aed23475d8471a046d5
Adi Eyal <adi@burgercom.co.za>
2013-09-15 00:11:11 +0200

    Updated fabfile to use rsync

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,11 @@
 from fabric.api import put, env
+from fabric.contrib.project import rsync_project
 
 env.user = 'adi'
 # the servers where the commands are executed
 env.hosts = ['197.221.34.5:2222']
 
 def deploy():
-    put('_site/*', '/var/www/www.code4sa.org/')
+    rsync_project("/var/www/www.code4sa.org/", "_site/")
+    #put('_site/*', '/var/www/www.code4sa.org/')
     
