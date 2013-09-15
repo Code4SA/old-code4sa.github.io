d1764aa0160fe50e9398878fa51297d1cd8eebbf
Adi Eyal <adi@burgercom.co.za>
2013-09-15 08:12:55 +0200

    Improved deploy script to generate html and delete remote

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import put, env
+from fabric.api import put, env, local
 from fabric.contrib.project import rsync_project
 
 env.user = 'adi'
@@ -6,6 +6,7 @@ env.user = 'adi'
 env.hosts = ['197.221.34.5:2222']
 
 def deploy():
-    rsync_project("/var/www/www.code4sa.org/", "_site/")
+    local("jekyll build")
+    rsync_project("/var/www/www.code4sa.org/", "_site/", delete=True)
     #put('_site/*', '/var/www/www.code4sa.org/')
     
