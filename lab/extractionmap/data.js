133c5e7262c10fd0c381f2cad8789964b09a2a16
Adi Eyal <adi@code4sa.org>
2015-08-16 23:30:54 +0200

    Added parent page
    Also moved data into separate file

---
+++ lab/extractionmap/data.js
@@ -0,0 +1,56 @@
+var data = {
+    Botswana : {
+        body : 'Multiple Australian mining companies are accused of negligence, unfair dismissal, violence and environmental law-breaking across Africa, according to legal filings.',
+        image : 'image1.jpg'
+    },
+    Burkina_Faso : {
+
+        body : 'Australian state and federal government entities, including government workers’ pension funds, have invested in controversial Australian mining companies operating in Africa.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/adele%20looking_1600.jpg'
+    },
+    Ghana : {
+        body : 'A pattern of links between mining activities and deaths, disfigurement, environmental destruction and displacement suggests a troubling track record for Australian companies ',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/DickayKunda_1600.jpg'
+    },
+    Ivory_Coast : {
+        body : 'Broken promises to impoverished communities, serious environmental concerns and poor health and safety records linked to Australian mining companies have all been revealed by Africa’s largest ever collaborative journalistic investigation.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/DRC_ominous_clouds_1600.jpg'
+    },
+    Madagascar : {
+        body : 'Since the beginning of 2004, more than 380 people have died in mining accidents or in off-site skirmishes connected to Australian publicly-traded mining companies in 13 countries in Africa.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/soldiers_1600.jpg'
+    },
+    Malawi : {
+        body : 'At the end of 2014, more than 150 companies held about 1500 licenses and owned or managed dozens of mining operations across 33 countries in Africa.',
+        image : 'malawi_burn.jpg'
+    },
+    Mali : {
+        body : 'The 18-month Fatal Extraction investigation has brought together data and records from Australia and Africa in a way that hasn’t been done before, combining corporate information from the Australian Securities Exchange (ASX) with months of on-the-ground work by reporters throughout Africa.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/Kilwaport_1600.jpg'
+    },
+    Namibia : {
+        body : 'Australian mining companies are linked to hundreds of deaths and injuries in Africa, which can go unreported at home.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/DRCanvil_signage1_1600.jpg'
+    },
+    Niger : {
+        body : 'The fact that Paladin evacuated Mr. Sichinga and his co-workers to Millpark Hospital in Johannesburg, South Africa, regarded as one of the finest medical facilities in Africa for the treatment of burns cases, is evidence that Paladin made no distinction based on race',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/Prison.Still001%20edit_1600.jpg'
+    },
+    Senegal : {
+        body : 'Australian mining companies are more numerous than those from other mining giants such as Canada, the United Kingdom and China.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/SA_Ikemelang_barbed_wire_1600.jpg'
+    },
+    South_Africa : {
+        body : 'The use of contract labor creates a complex health and safety environment. In the case of injuries, individuals can be stuck between two companies arguing over liability.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/aerial3_1600.jpg',
+    },
+    Tanzania : {
+        body : 'Contract labor as a percentage of the mining workforce in South Africa is particularly high. On a per-hours-worked basis, the mining fatality rate in South Africa is four times Australia’s, according to figures from the South African Chamber of Mines.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/drc_Kilwa_street_scene_1600.jpg'
+    },
+    Zambia : {
+        body : 'There is no need to employ more than one individual in this area since continual cleaning by one person is sufficient to keep the area clean. Furthermore, this was not an isolated area and Mr. Mhlathuszana was not isolated at this work station.',
+        image : 'http://cloudfront-files-1.publicintegrity.org/apps/2015/04/africamining/media/03_IMAGES/miners_centered_1600.jpg'
+    },
+};
+
