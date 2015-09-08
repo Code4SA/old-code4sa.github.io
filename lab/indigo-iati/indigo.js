32cb405915bc8711f3b2f32c5ed814820252afa6
Adi Eyal <adi@code4sa.org>
2015-09-08 10:25:47 +0200

    Updated code to work with new data

--- lab/indigo-iati/indigo.js
+++ lab/indigo-iati/indigo.js
@@ -6,6 +6,24 @@ var x2js = new X2JS();
 var start_date = new Date(2010, 6, 1)
 var end_date = new Date(2016, 1, 1)
 
+var date_format = d3.time.format("%Y-%m-%d")
+var get_start_date = function(d) {
+    var activity_date = d["activity-date"]
+    console.log(activity_date)
+    for (i in [0, 1]) {
+        if (
+            activity_date[i] != undefined &&
+            (
+                activity_date[i]["_type"] == "start-actual" ||
+                activity_date[i]["_type"] == "start-planned"
+            )
+        ) {
+            return date_format.parse(d["activity-date"][i]["_iso-date"])
+        }
+    }
+    return date_format.parse(d["activity-date"]["_iso-date"])
+}
+
 var get_country = function(d) {
     var country = "";
     if (d['recipient-country'] != undefined)
@@ -21,18 +39,41 @@ var get_organisation = function(d) {
     return "No organisation found"
 }
 
-var get_country_color = function(d) {
-    var country = get_country(d);
-    if (country == "UG")
-        return "red"
-    return "#eee"
-
+var rand_pound = 19;
+var dollar_pound = rand_pound / 14;
+var convert_to_pounds = function(value) {
+    var val = parseInt(value["__text"])
+    if (value["_currency"] == "GBP" || value["_currency"] == undefined)
+        return val
+    else if (value["_currency"] == "ZAR")
+        return val / rand_pound;
+    else if (value["_currency"] == "USD") {
+        return val / dollar_pound
+    } else {
+        console.log(value)
+        return 20000
+    }
 }
 
 var get_budget = function(d) {
-    if (d["budget"] == undefined)
-        return d["transaction"]["value"]
-    return money_scale(parseInt(d["budget"]["value"]["__text"]))
+    if (d["transaction"] != undefined) {
+        if (d["transaction"]["value"] != undefined) {
+            var transaction = d["transaction"]["value"]
+            return convert_to_pounds(transaction)
+        }
+        else if (d["transaction"].length != undefined) {
+            var total = 0;
+            for (el in d["transaction"]) {
+                var transaction = d["transaction"][el]
+                total += convert_to_pounds(transaction["value"])
+            }
+            return total
+        }
+    }
+    else {
+        var budget = d["budget"]["value"]
+        return convert_to_pounds(budget)
+    }
 }
 
 var color_scale = d3.scale.ordinal()
@@ -47,28 +88,12 @@ var x_axis = d3.svg.axis()
     .tickSize(0.4)
     .tickPadding(8);
 
-var money_scale = d3.scale.linear().domain([0, 60000]).range([height - margin.top, 0 + margin.bottom])
 var svg = d3.select('#chart').append('svg')
     .attr('class', 'chart')
     .attr("viewBox", "0 0 1500 800")
     .append('g')
         .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-var y_axis = d3.svg.axis()
-    .scale(money_scale)
-    .orient('left')
-    .tickSize(0)
-    .tickPadding(8);
-
-svg.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
-    .call(x_axis);
-
-svg.append('g')
-    .attr('class', 'y axis')
-    .attr('transform', 'translate(0, ' + -margin.bottom + ')')
-    .call(y_axis)
 
 var paint_circles = function(d) {
     var country_code = get_country(d)
@@ -95,7 +120,8 @@ var tip = d3.tip()
 
         return "<dl class='dl-horizontal'>" +
             "<dt>Title:</dt><dd>" + d.title + "</dd>" +
-            "<dt>Value:</dt><dd>&pound;" + d.budget.value + "</dd>" +
+            "<dt>Value:</dt><dd>&pound;" + d3.format(',0d')(get_budget(d)) + "</dd>" +
+            "<dt>Start date:</dt><dd>" + d3.time.format("%Y-%m-%d")(get_start_date(d)) + "</dd>" +
             "<dt>Recipient:</dt><dd>" + get_organisation(d) + "</dd>" +
             "<dt>Country:</dt><dd>" + get_country(d) + "</dd>" +
         "</dl>" +
@@ -108,6 +134,24 @@ d3.xml("indtrust-activities.xml", function(xml) {
     
     var json = x2js.xml2json(xml)
     var data = json["iati-activities"]["iati-activity"]
+    var max_val = d3.max(data, get_budget)
+    var money_scale = d3.scale.linear().domain([0, max_val]).range([height - margin.top, 0 + margin.bottom])
+    var y_axis = d3.svg.axis()
+        .scale(money_scale)
+        .orient('left')
+        .tickSize(0)
+        .tickPadding(8);
+
+    svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+        .call(x_axis);
+
+    svg.append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(0, ' + -margin.bottom + ')')
+        .call(y_axis)
+
     svg
         .append("g")
             .attr("transform", "translate(0, " + -margin.bottom + ")")
@@ -117,12 +161,10 @@ d3.xml("indtrust-activities.xml", function(xml) {
             .append("circle")
                 .attr("r", 5)
                 .attr("cx", function(d) {
-                    if (d["activity-date"][0])
-                        return time_scale(Date.parse(d["activity-date"][0]["_iso-date"]))
-                    return time_scale(Date.parse(d["activity-date"]["_iso-date"]))
+                    return time_scale(get_start_date(d))
                 })
                 .attr("cy", function(d) {
-                    return get_budget(d)
+                    return money_scale(get_budget(d))
                     
                 })
                 .attr("data-code", get_country)
@@ -134,7 +176,6 @@ d3.xml("indtrust-activities.xml", function(xml) {
         d3.selectAll("circle")
             .each(function() {
                 var me2 = d3.select(this)
-                console.log(me.attr("data-code"))
                 if (me.attr("data-code") == me2.attr("data-code"))
                     me2.classed("selected", true)
                 else
